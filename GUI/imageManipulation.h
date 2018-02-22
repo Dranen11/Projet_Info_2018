2d6f3c8121ffddb3e665231a8fdda807102d9dc0
Mathieu de Bony <mathieu.de-bony-de-lavergne@u-psud.fr>
2018-02-23 00:16:38 +0100

    Bug correction

--- GUI/imageManipulation.h
+++ GUI/imageManipulation.h
@@ -7,7 +7,7 @@
 unsigned char* convertImageRGB32(std::vector<std::vector<vecteur<double,3>>> const& image, double min = 0., double max = -1., std::size_t alignement = 4);
 std::vector<std::vector<double>> calculateLuminosity(std::vector<std::vector<vecteur<double,3>>> const& image);
 void imageMinMax(std::vector<std::vector<double>> const& luminosity, double& min, double& max);
-vecteur<double, 3> calculateColor(vecteur<double,3> const& luminosity, double const& totalLuminosity);
+vecteur<double, 3> calculateColor(vecteur<double,3> const& luminosity);
 
 
 #endif // IMAGEMANIPULATION_H
